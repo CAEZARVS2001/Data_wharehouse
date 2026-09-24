0fda98dc402c7bfe6932f2ee09ac10574ab17a0e
CAEZARVS2001 <donselley0105@gmail.com>
2026-09-24 03:13:00 -0600

    fix: Added FK to dimensions customers and date for factual tables payments and reviews, also added tests

--- olist_dbt/models/marts/fact_payments.sql
+++ olist_dbt/models/marts/fact_payments.sql
@@ -1,13 +1,30 @@
 WITH source AS
 (
     SELECT
+        p.order_id,
+        p.payment_sequential,
+        o.customer_id,
+        TO_CHAR(o.order_purchase_ts::DATE, 'YYYYMMDD')::INT AS purchase_date_key,
+        p.payment_type,
+        p.payment_installments,
+        p.payment_value
+    FROM {{  ref('stg_order_payments')  }} AS p
+    LEFT JOIN {{  ref('stg_orders')  }} AS o
+        ON p.order_id = o.order_id
+),
+
+final AS
+(
+    SELECT  
         order_id,
         payment_sequential,
+        customer_id,
+        purchase_date_key,
         payment_type,
         payment_installments,
         payment_value
-    FROM {{  ref('stg_order_payments')  }}
+    FROM source
 )
 
 SELECT *
-FROM source
+FROM final
--- olist_dbt/models/marts/fact_reviews.sql
+++ olist_dbt/models/marts/fact_reviews.sql
@@ -1,9 +1,34 @@
-SELECT
-    review_id,
-    order_id,
-    review_score,
-    review_comment_title,
-    review_comment_message,
-    review_creation_ts,
-    review_answer_ts
-FROM {{ ref('stg_order_reviews') }}
+WITH source AS
+(
+    SELECT
+        r.review_id,
+        r.order_id,
+        o.customer_id,
+        TO_CHAR(r.review_creation_ts::DATE, 'YYYYMMDD')::INTEGER AS review_date_key,
+        r.review_score,
+        r.review_comment_title,
+        r.review_comment_message,
+        r.review_creation_ts,
+        r.review_answer_ts
+    FROM {{ ref('stg_order_reviews') }} AS r
+    LEFT JOIN {{  ref('stg_orders')  }} AS o
+        ON r.order_id = o.order_id
+),
+
+final AS
+(
+    SELECT
+        review_id,
+        order_id,
+        customer_id,
+        review_date_key,
+        review_score,
+        review_comment_title,
+        review_comment_message,
+        review_creation_ts,
+        review_answer_ts
+    FROM source
+)
+
+SELECT *
+FROM final
